9829bb5258ad3c83bbb783503924f9169e2081b4
Caio Guedes <caio.cesar.g.souza@gmail.com>
2019-07-12 09:49:17 -0300

    Improve PatternParserFactory

--- src/main/java/com/guedes/parser/pattern/PatternParserFactory.java
+++ src/main/java/com/guedes/parser/pattern/PatternParserFactory.java
@@ -1,23 +1,19 @@
 package com.guedes.parser.pattern;
 
-import java.util.regex.Pattern;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PatternParserFactory {
+  private static final List<PatternParser> parsers = Arrays.asList(
+      new CustomerPatternParser(),
+      new SellerPatternParser(),
+      new SalePatternParser()
+  );
+
   public PatternParser create(String text) {
-    if (Pattern.matches(CustomerPatternParser.LINE_PATTERN, text)) {
-      return new CustomerPatternParser();
-    }
-
-    if (Pattern.matches(SellerPatternParser.LINE_PATTERN, text)) {
-      return new SellerPatternParser();
-    }
-
-    if (Pattern.matches(SalePatternParser.LINE_PATTERN, text)) {
-      return new SalePatternParser();
-    }
-
-    throw new RuntimeException("No parser was found.");
+    return parsers.stream().filter(parse -> parse.check(text)).findFirst().orElseThrow(() ->
+        new RuntimeException("No parser was found."));
   }
 }
